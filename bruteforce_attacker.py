075874c59d009c1d4d2e827beeac4ac28817e5ef
Vishnu Ramineni <vishnu.ramineni.05@gmail.com>
2022-03-09 22:20:51 -0600

    added readme

--- bruteforce_attacker.py
+++ bruteforce_attacker.py
@@ -4,10 +4,14 @@ from utils_demo import *
 from Cryptodome.Cipher import AES
 from base64 import b64encode, b64decode
 
+#intializing the values
 number_of_keys = 0
 key_found = None
 key_available = threading.Event()
+key_space=24
+bytes_length=3
 
+# Reading the first 13 bytes from the generated key
 key_file = open("key.bin", "rb")
 data = key_file.read()
 formatted_hex_value=hex(int(data, 2))
@@ -15,13 +19,15 @@ hex_value=formatted_hex_value.split('x')
 byte_array=bytes.fromhex(hex_value[1])
 fixed_key_bytes=byte_array[0:13]
 fixed_key_pad = bytearray(fixed_key_bytes)
-key_space=24
-bytes_length=3
-    
+
+# Performing the bruteforce attack
 def key_finder(message,cipher,nonce):
     global number_of_keys
+    #mentioned key space limited to 24 bits and the range will be 2^n-1 where i starts from 0.
     for i in range(2**key_space-1):
+        #generating the key with first 13 bytes and random key pad
         generated_key = fixed_key_pad + i.to_bytes(bytes_length,byteorder='big')
+        #using the generated key to decrypt the cipher to obtain the message
         message_bytes = decryptor_CTR(cipher, nonce, generated_key)
         if string_to_bytes(message) == message_bytes:
             key_found = "Key for message: "+message+"\n"+"key: "+generated_key.hex()
@@ -31,6 +37,7 @@ def key_finder(message,cipher,nonce):
 def bruteforce_for_keys():
     for n in range(1,4):
         try:
+            #reading the data from the files iteratively
             message = read_file("m"+str(n)+".txt")
             cipher_bytes = read_bytes("c"+str(n)+".bin")
             nonce_bytes = read_bytes("nonce"+str(n)+".bin")
@@ -41,6 +48,7 @@ def bruteforce_for_keys():
             print('')
 
 def main():
+    #implemented threading to execute the tasks one after the other
     thread = threading.Thread(target=bruteforce_for_keys)
     thread.start()
     while not key_available.wait(timeout=5):
